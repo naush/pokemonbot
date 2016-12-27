4d373fae0b718c9dc865562b06ac01a27248d0b5
Li-Hsuan Lung <lihsuan@8thlight.com>
2016-12-27 10:20:02 -0500

    Return random pokemon

--- pokemonbot.py
+++ pokemonbot.py
@@ -1,30 +1,17 @@
 import os
 import time
 from slackclient import SlackClient
+from pokemonbot.commands import whois
 
-# starterbot's ID as an environment variable
+BOT_TOKEN = os.environ.get('SLACK_BOT_TOKEN')
 BOT_ID = os.environ.get("BOT_ID")
-
-# constants
 AT_BOT = "<@" + BOT_ID + ">"
-EXAMPLE_COMMAND = "do"
 
-# instantiate Slack & Twilio clients
-slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+slack_client = SlackClient(BOT_TOKEN)
 
-def handle_command(command, channel):
-    """
-        Receives commands directed at the bot and determines if they
-        are valid commands. If so, then acts on the commands. If not,
-        returns back what it needs for clarification.
-    """
-    response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
-               "* command with numbers, delimited by spaces."
-    if command.startswith(EXAMPLE_COMMAND):
-        response = "Sure...write some more code then I can do that!"
+def handle_command(user_name, command, channel):
     slack_client.api_call("chat.postMessage", channel=channel,
-                          text=response, as_user=True)
-
+                          text=whois.respond(user_name, command), as_user=True)
 
 def parse_slack_output(slack_rtm_output):
     """
@@ -36,19 +23,23 @@ def parse_slack_output(slack_rtm_output):
     if output_list and len(output_list) > 0:
         for output in output_list:
             if output and 'text' in output and AT_BOT in output['text']:
-                # return text after the @ mention, whitespace removed
-                return output['text'].split(AT_BOT)[1].strip().lower(), \
+                return output['user'], output['text'].split(AT_BOT)[1].strip().lower(), \
                        output['channel']
-    return None, None
+    return None, None, None
 
 if __name__ == "__main__":
     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
     if slack_client.rtm_connect():
-        print("PokemonBot connected and running!")
-        while True:
-            command, channel = parse_slack_output(slack_client.rtm_read())
-            if command and channel:
-                handle_command(command, channel)
-            time.sleep(READ_WEBSOCKET_DELAY)
+        api_call = slack_client.api_call("users.list")
+        if api_call.get('ok'):
+            users = api_call.get('members')
+            print("PokemonBot connected and running!")
+            while True:
+                user_id, command, channel = parse_slack_output(slack_client.rtm_read())
+                if user_id and command and channel:
+                    for user in users:
+                        if 'id' in user and user.get('id') == user_id:
+                            handle_command(user.get('name'), command, channel)
+                time.sleep(READ_WEBSOCKET_DELAY)
     else:
         print("Connection failed. Invalid Slack token or bot ID?")
