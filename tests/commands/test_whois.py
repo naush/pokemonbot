ecd189971be2740e7cc336ce311e96817a75911b
Li-Hsuan Lung <lihsuan@8thlight.com>
2016-12-27 10:52:48 -0500

    Add whois command test cases

--- tests/commands/test_whois.py
+++ tests/commands/test_whois.py
@@ -1,5 +1,17 @@
-from pokemonbot.commands import whois
 import pytest
+from unittest import mock
 
-def test_whois():
+from pokemonbot.commands import whois
+
+def test_whois_standard_response():
     assert whois.respond('John', 'hello') == 'Do you want to know what kind of pokemon is John? Ask me *who is* John.'
+
+@mock.patch('random.choice')
+def test_whois_random_pokemon(mocked_random_choice):
+    mocked_random_choice.return_value = 'pikachu'
+    assert whois.respond('John', 'who is John') == 'John is a :pokemon-pikachu:!'
+
+@mock.patch('random.choice')
+def test_whois_strips_non_alphanumerical_character_from_user_name(mocked_random_choice):
+    mocked_random_choice.return_value = 'pikachu'
+    assert whois.respond('John', 'who is Mary?') == 'Mary is a :pokemon-pikachu:!'
