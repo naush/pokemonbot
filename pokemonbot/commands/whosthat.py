ffd47106a99ad74d2510386635ac7be0864db0d0
Li-Hsuan Lung <lihsuan@8thlight.com>
2016-12-29 19:22:34 -0500

    More negative feedback

--- pokemonbot/commands/whosthat.py
+++ pokemonbot/commands/whosthat.py
@@ -5,8 +5,6 @@ from pokemonbot.database.interface import load, save
 COMMAND = "who’s that pokemon"
 
 def respond(user_id, user_input):
-    response = 'Nope.'
-
     data = load('pokemonbot/database/pokemons.json')
     pokemon_names = [pokemon['name'] for pokemon in data['pokemons']]
     users = load('pokemonbot/database/users.json')
@@ -21,11 +19,24 @@ def respond(user_id, user_input):
         if pokemon in user_input:
             response = 'You got it!'
             user_data = {}
+        else:
+            attempt = user_data['context']['attempt'] + 1
+            user_data['context']['attempt'] = attempt
+
+            if attempt < 3:
+                response = 'Nope.'
+            elif attempt < 4:
+                response = 'Are you even a Pokémon fan?'
+            elif attempt < 5:
+                response = 'Last chance.'
+            else:
+                response = 'It’s a ' + pokemon + '. Go read a Pokédex.'
+                user_data = {}
     else:
         pokemon = random.choice(pokemon_names)
-        user_data['context'] = {'pokemon':pokemon}
+        user_data['context'] = {'pokemon':pokemon, 'attempt':0}
         user_data['command'] = COMMAND
-        response = 'Who is :pokemon-' + pokemon + ':?'
+        response = 'Who is that pokemon? :pokemon-' + pokemon + ':'
 
     users[user_id] = user_data
     save(users)
--- tests/commands/test_whosthat.py
+++ tests/commands/test_whosthat.py
@@ -7,30 +7,52 @@ from pokemonbot.commands import whosthat
 @mock.patch('pokemonbot.commands.whosthat.save')
 def test_whosthat_new_game(mocked_save, mocked_load):
     mocked_load.side_effect = [{'pokemons':[{'name':'pikachu'}]}, {}]
-    assert whosthat.respond('U21EYJH0T', "who's that pokemon") == 'Who is :pokemon-pikachu:?'
+    assert whosthat.respond('U21EYJH0T', "who's that pokemon") == 'Who is that pokemon? :pokemon-pikachu:'
 
 @mock.patch('pokemonbot.commands.whosthat.load')
 @mock.patch('pokemonbot.commands.whosthat.save')
 def test_whosthat_save_choice(mocked_save, mocked_load):
     mocked_load.side_effect = [{'pokemons':[{'name':'pikachu'}]}, {}]
     whosthat.respond('U21EYJH0T', whosthat.COMMAND)
-    mocked_save.assert_called_with({'U21EYJH0T':{'command':whosthat.COMMAND, 'context':{'pokemon':'pikachu'}}})
+    mocked_save.assert_called_with({'U21EYJH0T':{'command':whosthat.COMMAND, 'context':{'pokemon':'pikachu','attempt':0}}})
 
 @mock.patch('pokemonbot.commands.whosthat.load')
 @mock.patch('pokemonbot.commands.whosthat.save')
 def test_whosthat_guess_wrong(mocked_save, mocked_load):
     mocked_load.side_effect = [
         {'pokemons':[{'name':'pikachu'}]},
-        {'U21EYJH0T':{'context':{'pokemon':'pikachu'}}}
+        {'U21EYJH0T':{'context':{'pokemon':'pikachu','attempt':0}}}
     ]
     assert whosthat.respond('U21EYJH0T', "raichu") == 'Nope.'
+
+@mock.patch('pokemonbot.commands.whosthat.load')
+@mock.patch('pokemonbot.commands.whosthat.save')
+def test_whosthat_guess_wrong_again(mocked_save, mocked_load):
+    mocked_load.side_effect = [
+        {'pokemons':[{'name':'pikachu'}]},
+        {'U21EYJH0T':{'context':{'pokemon':'pikachu','attempt':2}}},
+        {'U21EYJH0T':{'context':{'pokemon':'pikachu','attempt':3}}},
+        {'U21EYJH0T':{'context':{'pokemon':'pikachu','attempt':4}}},
+    ]
+    assert whosthat.respond('U21EYJH0T', "raichu") == 'Are you even a Pokémon fan?'
+    assert whosthat.respond('U21EYJH0T', "pidgey") == 'Last chance.'
+    assert whosthat.respond('U21EYJH0T', "rattata") == 'It’s a pikachu. Go read a Pokédex.'
+
+@mock.patch('pokemonbot.commands.whosthat.load')
+@mock.patch('pokemonbot.commands.whosthat.save')
+def test_whosthat_guess_wrong_again(mocked_save, mocked_load):
+    mocked_load.side_effect = [
+        {'pokemons':[{'name':'pikachu'}]},
+        {'U21EYJH0T':{'context':{'pokemon':'pikachu','attempt':3}}}
+    ]
+    assert whosthat.respond('U21EYJH0T', "raichu") == 'Last chance.'
 
 @mock.patch('pokemonbot.commands.whosthat.load')
 @mock.patch('pokemonbot.commands.whosthat.save')
 def test_whosthat_guess_right(mocked_save, mocked_load):
     mocked_load.side_effect = [
         {'pokemons':[{'name':'pikachu'}]},
-        {'U21EYJH0T':{'context':{'pokemon':'pikachu'}}}
+        {'U21EYJH0T':{'context':{'pokemon':'pikachu','attempt':0}}}
     ]
     assert whosthat.respond('U21EYJH0T', "pikachu") == 'You got it!'
 
@@ -39,7 +61,17 @@ def test_whosthat_guess_right(mocked_save, mocked_load):
 def test_whosthat_clears_user_data(mocked_save, mocked_load):
     mocked_load.side_effect = [
         {'pokemons':[{'name':'pikachu'}]},
-        {'U21EYJH0T':{'context':{'pokemon':'pikachu'}}}
+        {'U21EYJH0T':{'context':{'pokemon':'pikachu','attempt':0}}}
     ]
     whosthat.respond('U21EYJH0T', "pikachu")
     mocked_save.assert_called_with({'U21EYJH0T':{}})
+
+@mock.patch('pokemonbot.commands.whosthat.load')
+@mock.patch('pokemonbot.commands.whosthat.save')
+def test_whosthat_counts_number_of_attemps(mocked_save, mocked_load):
+    mocked_load.side_effect = [
+        {'pokemons':[{'name':'pikachu'}]},
+        {'U21EYJH0T':{'command':whosthat.COMMAND, 'context':{'pokemon':'pikachu','attempt':0}}}
+    ]
+    whosthat.respond('U21EYJH0T', "pidgey")
+    mocked_save.assert_called_with({'U21EYJH0T':{'command':whosthat.COMMAND, 'context':{'pokemon':'pikachu','attempt':1}}})
